0bca2c3da7ed75733058ce04e99765c670998164
Sergey Vidishev <ft.priest@gmail.com>
2024-07-15 14:46:51 +0300

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Сергей on 15.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
